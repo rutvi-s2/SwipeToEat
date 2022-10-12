625bafadc276e0d07051fb6fc0d00d85753a0f3a
rutvi-s2 <rutvi01.shah@gmail.com>
2022-10-12 09:42:01 -0500

    Created SwipeToEat Repo - Rutvi Shah

--- app/src/main/java/com/example/swipetoeat/MainActivity.kt
+++ app/src/main/java/com/example/swipetoeat/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.swipetoeat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//SwipeToEat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
