e1e1ff6559175edf40f2ddc780ecf13c8ee22781
nab0000 <abbynguyen@utexas.edu>
2022-10-25 18:32:23 -0500

    added navigation for home and swipe page

--- app/src/main/java/com/example/swipetoeat/MainActivity.kt
+++ app/src/main/java/com/example/swipetoeat/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.swipetoeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.content.Intent
 
 //SwipeToEat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // pushing to git2
+        // Moves the user to the second page to start swiping
+        val swipePage = findViewById<ImageButton>(R.id.start_swiping)
+        swipePage.setOnClickListener {
+            val intent = Intent(this,SwipeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/swipetoeat/SwipeActivity.kt
+++ app/src/main/java/com/example/swipetoeat/SwipeActivity.kt
@@ -1,4 +1,20 @@
 package com.example.swipetoeat
 
-class SwipeActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.content.Intent
+
+//SwipeToEat
+class SwipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe)
+        // Moves the user to the second page to start swiping
+        val homePage = findViewById<ImageButton>(R.id.home_button)
+        homePage.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
