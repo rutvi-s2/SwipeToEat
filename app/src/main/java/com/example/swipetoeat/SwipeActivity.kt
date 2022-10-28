7d6c1a513252be26451fc88bbf0cb20ea101b345
rutvi-s2 <rutvi01.shah@gmail.com>
2022-10-28 09:42:58 -0500

    show appropriate toast when swiping left/right

--- app/src/main/java/com/example/swipetoeat/SwipeActivity.kt
+++ app/src/main/java/com/example/swipetoeat/SwipeActivity.kt
@@ -8,9 +8,11 @@ import android.widget.ImageButton
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.widget.Toast
 import com.example.swipetoeat.data.DataSource
 import com.example.swipetoeat.databinding.ActivitySwipeBinding
 import com.example.swipetoeat.model.Restaurant
+import com.yalantis.library.KolodaListener
 
 
 class SwipeActivity : AppCompatActivity() {
@@ -48,10 +50,27 @@ class SwipeActivity : AppCompatActivity() {
         adapter = SwipeAdapter(this, list)
         koloda.adapter = adapter
 
+        koloda.kolodaListener = object : KolodaListener {
+
+            override fun onCardSwipedLeft(position: Int) {
+                val text = "Swiped Left!"
+                val duration = Toast.LENGTH_SHORT
+
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+
+            override fun onCardSwipedRight(position: Int) {
+                val text = "Swiped Right!"
+                val duration = Toast.LENGTH_SHORT
+
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+
+        }
+
 //        imageResourceBitmap = BitmapFactory.decodeResource(resources, R.drawable.casa_de_mariscos_enchiladas);
-
-
-
 
         // Intent takes user to the find restaurants page once they are done swiping
         val restaurantsPage = findViewById<ImageButton>(R.id.done_swiping_button)
